cee3b8676c938f6277c19e4c44bec46d2921127e
alexandresalvatierra <alexandre.salvatierra@gmail.com>
2020-05-04 22:06:32 -0300

    feat: add customer validation

--- FormBehavior/FormBehavior/Customer.Designer.cs
+++ FormBehavior/FormBehavior/Customer.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
             this.txtName = new System.Windows.Forms.TextBox();
             this.txtEmail = new System.Windows.Forms.TextBox();
@@ -37,40 +38,33 @@
             this.txtConPass = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.btnSave = new System.Windows.Forms.Button();
-            this.chkTerms = new System.Windows.Forms.CheckBox();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.radioButton3 = new System.Windows.Forms.RadioButton();
-            this.radioButton4 = new System.Windows.Forms.RadioButton();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.radioButton6 = new System.Windows.Forms.RadioButton();
-            this.radioButton7 = new System.Windows.Forms.RadioButton();
-            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.mktPostalCode = new System.Windows.Forms.MaskedTextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
-            this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
-            this.panel1.SuspendLayout();
-            this.panel2.SuspendLayout();
+            this.mktCnpj = new System.Windows.Forms.MaskedTextBox();
+            this.erpCustomer = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.erpCustomer)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(49, 26);
+            this.label1.Location = new System.Drawing.Point(49, 30);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(42, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Name";
+            this.label1.Text = "Name *";
             // 
             // txtName
             // 
-            this.txtName.Location = new System.Drawing.Point(52, 43);
+            this.txtName.Location = new System.Drawing.Point(52, 47);
             this.txtName.Name = "txtName";
             this.txtName.Size = new System.Drawing.Size(100, 20);
             this.txtName.TabIndex = 0;
             // 
             // txtEmail
             // 
-            this.txtEmail.Location = new System.Drawing.Point(203, 43);
+            this.txtEmail.Location = new System.Drawing.Point(200, 47);
             this.txtEmail.Name = "txtEmail";
             this.txtEmail.Size = new System.Drawing.Size(100, 20);
             this.txtEmail.TabIndex = 1;
@@ -78,15 +72,15 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(200, 26);
+            this.label2.Location = new System.Drawing.Point(197, 30);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(42, 13);
             this.label2.TabIndex = 2;
-            this.label2.Text = "E-mail";
+            this.label2.Text = "E-mail *";
             // 
             // txtPass
             // 
-            this.txtPass.Location = new System.Drawing.Point(52, 107);
+            this.txtPass.Location = new System.Drawing.Point(52, 97);
             this.txtPass.Name = "txtPass";
             this.txtPass.Size = new System.Drawing.Size(100, 20);
             this.txtPass.TabIndex = 2;
@@ -94,7 +88,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(49, 90);
+            this.label3.Location = new System.Drawing.Point(49, 80);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(53, 13);
             this.label3.TabIndex = 4;
@@ -102,7 +96,7 @@
             // 
             // txtConPass
             // 
-            this.txtConPass.Location = new System.Drawing.Point(203, 107);
+            this.txtConPass.Location = new System.Drawing.Point(200, 97);
             this.txtConPass.Name = "txtConPass";
             this.txtConPass.Size = new System.Drawing.Size(100, 20);
             this.txtConPass.TabIndex = 3;
@@ -110,7 +104,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(200, 90);
+            this.label4.Location = new System.Drawing.Point(197, 80);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(91, 13);
             this.label4.TabIndex = 6;
@@ -118,97 +112,26 @@
             // 
             // btnSave
             // 
-            this.btnSave.Location = new System.Drawing.Point(304, 337);
+            this.btnSave.Location = new System.Drawing.Point(240, 205);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(75, 23);
             this.btnSave.TabIndex = 8;
             this.btnSave.Text = "Save";
             this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
-            // chkTerms
+            // mktPostalCode
             // 
-            this.chkTerms.AutoSize = true;
-            this.chkTerms.Location = new System.Drawing.Point(304, 299);
-            this.chkTerms.Name = "chkTerms";
-            this.chkTerms.Size = new System.Drawing.Size(55, 17);
-            this.chkTerms.TabIndex = 11;
-            this.chkTerms.Text = "Terms";
-            this.chkTerms.UseVisualStyleBackColor = true;
-            // 
-            // panel1
-            // 
-            this.panel1.Controls.Add(this.radioButton3);
-            this.panel1.Controls.Add(this.radioButton4);
-            this.panel1.Location = new System.Drawing.Point(39, 193);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(231, 76);
-            this.panel1.TabIndex = 14;
-            // 
-            // radioButton3
-            // 
-            this.radioButton3.AutoSize = true;
-            this.radioButton3.Location = new System.Drawing.Point(134, 29);
-            this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(53, 17);
-            this.radioButton3.TabIndex = 15;
-            this.radioButton3.TabStop = true;
-            this.radioButton3.Text = "Music";
-            this.radioButton3.UseVisualStyleBackColor = true;
-            // 
-            // radioButton4
-            // 
-            this.radioButton4.AutoSize = true;
-            this.radioButton4.Location = new System.Drawing.Point(17, 29);
-            this.radioButton4.Name = "radioButton4";
-            this.radioButton4.Size = new System.Drawing.Size(55, 17);
-            this.radioButton4.TabIndex = 14;
-            this.radioButton4.TabStop = true;
-            this.radioButton4.Text = "Sports";
-            this.radioButton4.UseVisualStyleBackColor = true;
-            // 
-            // panel2
-            // 
-            this.panel2.Controls.Add(this.radioButton6);
-            this.panel2.Controls.Add(this.radioButton7);
-            this.panel2.Location = new System.Drawing.Point(39, 284);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(231, 76);
-            this.panel2.TabIndex = 16;
-            // 
-            // radioButton6
-            // 
-            this.radioButton6.AutoSize = true;
-            this.radioButton6.Location = new System.Drawing.Point(134, 29);
-            this.radioButton6.Name = "radioButton6";
-            this.radioButton6.Size = new System.Drawing.Size(52, 17);
-            this.radioButton6.TabIndex = 15;
-            this.radioButton6.TabStop = true;
-            this.radioButton6.Text = "Chery";
-            this.radioButton6.UseVisualStyleBackColor = true;
-            // 
-            // radioButton7
-            // 
-            this.radioButton7.AutoSize = true;
-            this.radioButton7.Location = new System.Drawing.Point(17, 29);
-            this.radioButton7.Name = "radioButton7";
-            this.radioButton7.Size = new System.Drawing.Size(60, 17);
-            this.radioButton7.TabIndex = 14;
-            this.radioButton7.TabStop = true;
-            this.radioButton7.Text = "Orange";
-            this.radioButton7.UseVisualStyleBackColor = true;
-            // 
-            // maskedTextBox1
-            // 
-            this.maskedTextBox1.Location = new System.Drawing.Point(52, 158);
-            this.maskedTextBox1.Mask = "00000-L00";
-            this.maskedTextBox1.Name = "maskedTextBox1";
-            this.maskedTextBox1.Size = new System.Drawing.Size(100, 20);
-            this.maskedTextBox1.TabIndex = 17;
+            this.mktPostalCode.Location = new System.Drawing.Point(52, 148);
+            this.mktPostalCode.Mask = "00000-L00";
+            this.mktPostalCode.Name = "mktPostalCode";
+            this.mktPostalCode.Size = new System.Drawing.Size(100, 20);
+            this.mktPostalCode.TabIndex = 4;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(52, 139);
+            this.label5.Location = new System.Drawing.Point(52, 129);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(98, 13);
             this.label5.TabIndex = 18;
@@ -217,32 +140,33 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(200, 139);
+            this.label6.Location = new System.Drawing.Point(200, 129);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(68, 13);
             this.label6.TabIndex = 20;
             this.label6.Text = "CNPJ (Brazil)";
             // 
-            // maskedTextBox2
+            // mktCnpj
             // 
-            this.maskedTextBox2.Location = new System.Drawing.Point(200, 158);
-            this.maskedTextBox2.Mask = "00.000.000/0000-00";
-            this.maskedTextBox2.Name = "maskedTextBox2";
-            this.maskedTextBox2.Size = new System.Drawing.Size(115, 20);
-            this.maskedTextBox2.TabIndex = 19;
+            this.mktCnpj.Location = new System.Drawing.Point(200, 148);
+            this.mktCnpj.Mask = "00.000.000/0000-00";
+            this.mktCnpj.Name = "mktCnpj";
+            this.mktCnpj.Size = new System.Drawing.Size(115, 20);
+            this.mktCnpj.TabIndex = 5;
+            // 
+            // erpCustomer
+            // 
+            this.erpCustomer.ContainerControl = this;
             // 
             // Customer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(416, 381);
+            this.ClientSize = new System.Drawing.Size(366, 259);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.maskedTextBox2);
+            this.Controls.Add(this.mktCnpj);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.maskedTextBox1);
-            this.Controls.Add(this.panel2);
-            this.Controls.Add(this.panel1);
-            this.Controls.Add(this.chkTerms);
+            this.Controls.Add(this.mktPostalCode);
             this.Controls.Add(this.btnSave);
             this.Controls.Add(this.txtConPass);
             this.Controls.Add(this.label4);
@@ -255,11 +179,8 @@
             this.KeyPreview = true;
             this.Name = "Customer";
             this.Text = "Customer";
-            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
-            this.panel2.ResumeLayout(false);
-            this.panel2.PerformLayout();
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Customer_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.erpCustomer)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -276,17 +197,11 @@
         private System.Windows.Forms.TextBox txtConPass;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button btnSave;
-        private System.Windows.Forms.CheckBox chkTerms;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.RadioButton radioButton3;
-        private System.Windows.Forms.RadioButton radioButton4;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.RadioButton radioButton6;
-        private System.Windows.Forms.RadioButton radioButton7;
-        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.MaskedTextBox mktPostalCode;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.MaskedTextBox maskedTextBox2;
+        private System.Windows.Forms.MaskedTextBox mktCnpj;
+        private System.Windows.Forms.ErrorProvider erpCustomer;
     }
 }
 
--- FormBehavior/FormBehavior/Customer.cs
+++ FormBehavior/FormBehavior/Customer.cs
@@ -17,13 +17,88 @@ namespace FormBehavior
             InitializeComponent();
         }
 
-        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        private void Customer_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.KeyCode == Keys.Enter)
             {
-                Console.WriteLine("Pressed keys enter");
+                //Console.WriteLine("Pressed keys enter");
                 SendKeys.Send("{TAB}");
             }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            //if (String.IsNullOrEmpty(txtName.Text.Trim())) {
+            //if (String.IsNullOrEmpty(txtName.Text)) {
+
+            //if (String.IsNullOrWhiteSpace(txtName.Text)) {
+
+            //if (txtName.Text.Length == 0) {
+            //if (txtName.Text.Trim().Length == 0) {
+
+            //if (txtName.Text == "") {
+            //if (txtName.Text.Trim() == "") {
+
+
+            if ( String.IsNullOrWhiteSpace( txtName.Text ) )
+            {
+                // MessageBox.Show("Name is required");
+                erpCustomer.SetError(txtName, "Name is required");
+
+                txtName.BackColor = Color.FromArgb(250, 128, 114);
+            }
+            else
+            {
+                erpCustomer.SetError(txtName, "");
+
+                txtName.BackColor = Color.FromName("Window");
+            }
+
+            if (String.IsNullOrWhiteSpace(txtEmail.Text))
+            {
+                erpCustomer.SetError(txtEmail, "E-mail is required");
+
+                txtEmail.BackColor = Color.FromArgb(250, 128, 114);
+            }
+            else
+            {
+                erpCustomer.SetError(txtEmail, "");
+
+                txtEmail.BackColor = Color.FromName("Window");
+            }
+
+            mktPostalCode.Mask = "";
+
+            if (mktPostalCode.Text == "     -")
+            //if (mktPostalCode.Mask == mktPostalCode.Text)
+            {
+                erpCustomer.SetError(mktPostalCode, "CEP is required");
+
+                mktPostalCode.BackColor = Color.FromArgb(250, 128, 114);
+            }
+            else
+            {
+                erpCustomer.SetError(mktPostalCode, "");
+
+                mktPostalCode.BackColor = Color.FromName("Window");
+            }
+
+            // = Atribuição 
+            // == Comparar valores
+            // != Diferente
+            if ( txtPass.Text != txtConPass.Text)
+            {
+                erpCustomer.SetError(txtConPass, "Passwords does not match");
+
+                txtConPass.BackColor = Color.FromArgb(250, 128, 114);
+            }
+            else
+            {
+                erpCustomer.SetError(txtConPass, "");
+
+                txtConPass.BackColor = Color.FromName("Window");
+            }
+
         }
     }
 }
